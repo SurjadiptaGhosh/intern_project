0a906e270ac443f853a4a4830ed5583e2c3c1c87
SURJADIPTA GHOSH <144163839+SurjadiptaGhosh@users.noreply.github.com>
2024-08-17 13:47:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my first html page</title>
+</head>
+<body>
+    <h1>Hello I am Surjadipta Ghosh.<h1>
+    <h3>I have completed my first task. <h3>    
+</body>
+</html>
